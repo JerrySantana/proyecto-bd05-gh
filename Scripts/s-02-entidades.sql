0224d2ef3934df00ec81e26ca98afb069bcfcf0d
Gerardo Santana <gs.amezcua@comunidad.unam.mx>
2024-04-24 14:45:46 -0600

    Comenzada la definicion de las tablas

--- Scripts/s-02-entidades.sql
+++ Scripts/s-02-entidades.sql
@@ -16,3 +16,16 @@ create table usuario(
   constraint  usuario_nombre_usuario_uk unique(nombre_usuario)
 );
 
+-- Definición de la tabla TIPO_SERVICIO
+create table tipo_servicio(
+  servicio_id number(10,0) not null,
+  descripcion varchar2(50) not null,
+  icono blob not null,
+  nombre varchar2(20) not null,
+  constraint tipo_servicio_pk primary key(servicio_id),
+  constraint tipo_servicio_nombre_uk unique(nombre)
+);
+
+-- Definición de la tabla ESTATUS
+
+-- Definición de la tabla VIVIENDA
